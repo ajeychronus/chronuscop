61f009b3aa03618e1c03474109a1bdb277ece5ea
ajey <ajey@chronus.com>
2011-06-24 17:13:50 +0530

    Finished using devise for user management.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Chronuscop::Application.configure do
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
+  # required for devise. Setting the mailer used by devise for sending account activation mails.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Chronuscop::Application.routes.draw do
+  devise_for :users
+
+  get "pages/index"
+
+  get "pages/about"
+
+  get "pages/contact"
+
+  get "pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +59,8 @@ Chronuscop::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => "pages#index"
+
 
   # See how all your routes lay out with "rake routes"
 
