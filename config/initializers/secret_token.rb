31783e5fb832bb65281a428671215483a0b56748
ajey <ajey@chronus.com>
2011-06-24 15:31:54 +0530

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chronuscop::Application.config.secret_token = '90a80fef3333fecc197f806090f122f41e7ab31028010a9f7d774db1fd1c9785670ae938cf9acb6e6c82745c8ff810e413b7b1b95450285ee95ac4a0181218a4'
