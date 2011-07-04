bbe7f23ec25b78d06c0fa29da1348f7c3d2f58e1
ajey <ajey@chronus.com>
2011-07-04 11:13:38 +0530

    Server now adds default keys and english default keys and text.

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -27,7 +27,7 @@ class TranslationsController < ApplicationController
     @translation = @project.translations.build()
   end
 
-  def add                       # action to add all translations.
+  def add                       # Action to add or update default translations.
     if(params[:translations_xml_string] == nil) then
       puts "Translations parameter missing."
     else
@@ -36,6 +36,8 @@ class TranslationsController < ApplicationController
       @project = Project.find(params[:project_id])
 
       translations_hash.each do |key,value|
+
+        # Handling the default translations.
         t = Translation.find_by_key(key)
         if ( t ) then
           t.value = value
@@ -46,6 +48,22 @@ class TranslationsController < ApplicationController
           @translation.key = key
           @translation.value = value
           if ! @translation.save then
+            puts "record could not be saved."
+          end
+        end
+
+        # Handling the english translations that will be added.
+        en_key = key.sub(/default/,"en")
+        t2 = Translation.find_by_key(en_key)
+
+        # If there is no such key then add it. If there is one
+        # then don't touch it.
+        if( ! t2 ) then
+          @en_translation = @project.translations.build()
+          @en_translation.key = en_key
+          @en_translation.value = value
+
+          if ! @en_translation.save then
             puts "record could not be saved."
           end
         end
